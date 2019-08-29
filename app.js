deaeee1934478d55fa9165f8a8fa6d71be63725a
kubadz <jakub.dziechciewicz@sap.com>
2019-08-29 11:32:01 +0200

    adjust the scenario to the use of id_token mutator

--- app.js
+++ app.js
@@ -4,21 +4,30 @@ const express = require('express');
 
 const app = express();
 
+const scopesMapping = new Map([
+  ['admin', 'read write'],
+  ['user', 'read'],
+  ['anonymous', '']
+])
+
 app.use(express.json())
 
 app.post('/', (req, res) => {
-  let body = req.body
-  console.log(`body received: ${JSON.stringify(body, null, 4)}`)
+  let body = req.body;
+  console.log(`body received: ${JSON.stringify(body, null, 4)}`);
 
-  body.extra = {
-    foo: 7,
-    boo: {
-      bar: "hello"
+  const group = req.header('X-group');
+
+  if (scopesMapping.has(group)) {
+    const scopes = scopesMapping.get(group)
+
+    body.extra = {
+      scopes: scopes
     }
   }
 
-  res.json(body)
-  console.log(`body sent: ${JSON.stringify(body, null, 4)}`)
+  res.json(body);
+  console.log(`body sent: ${JSON.stringify(body, null, 4)}`);
 });
 
 app.listen(8080);
